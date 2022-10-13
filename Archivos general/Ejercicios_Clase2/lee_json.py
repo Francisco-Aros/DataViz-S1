db6ceaf404d69893a31094bbe34a05d4d990a5cf
Francisco-Aros <franciscoarosmunoz@gmail.com>
2022-10-13 17:49:36 -0300

    archivos py para leer datos json

--- Archivos general/Ejercicios_Clase2/lee_json.py
+++ Archivos general/Ejercicios_Clase2/lee_json.py
@@ -1,6 +1,13 @@
-mi_json = open("datos.json","r")
-
+import json
+mi_json = open("datos.json","r", encoding = "UTF-8")
 print(mi_json)
 
+#al realizar la ejecución del archivo .py, da error, eso debido a la dirección del archivo
+#eso debe ser corregido cambiando el directorio abriendo un nuevo terminal y agregando cd “Archivos general\Ejercicios_Clase2” para este caso y
+# posteriormente, el comando python.exe lee_json.py
+
 json_datos = mi_json.read()
-print(json_datos)
+datos = json.loads(json_datos)
+print("json_datos:", json_datos)
+print("datos:", datos)
+print("temperaturas: ", datos["temperaturas"])
